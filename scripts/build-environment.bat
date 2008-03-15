2a8a86faf3ade75d827f096e05827b5dc372e45a
Steven Soloff <s_soloff@users.sourceforge.net>
2008-03-15 02:38:16 +0000

    Added Eclipse to build environment.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/build/trunk@59 897c5806-f142-0410-a293-e1bf5698dec8

--- scripts/build-environment.bat
+++ scripts/build-environment.bat
@@ -1,17 +1,18 @@
 @ECHO OFF
 
 REM
-SUBST g: "%HOMEDRIVE%%HOMEPATH%\My Documents\builds" 1> nul
+SUBST G: "%HOMEDRIVE%%HOMEPATH%\My Documents\builds" 1> nul
 
 REM
 SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_05
 SET SVN_HOME=C:\Program Files\Subversion
 SET ANT_HOME=C:\Program Files\apache-ant-1.7.0
 SET SVNANT_HOME=C:\Program Files\svnant-1.0.0
+SET ECLIPSE_HOME=C:\Program Files\eclipse-3.3.1.1
 SET CCDIR=C:\PROGRA~1\cruisecontrol-2.7.1\main
 
 REM
-SET PATH=%JAVA_HOME%\bin;%SVN_HOME%\bin;%ANT_HOME%\bin;%CCDIR%\bin;%PATH%
+SET PATH=%JAVA_HOME%\bin;%SVN_HOME%\bin;%ANT_HOME%\bin;%ECLIPSE_HOME%;%CCDIR%\bin;%PATH%
 
 G:
 
