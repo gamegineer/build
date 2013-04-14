ca601d23a903f449eb673c523e4623140c282739
Steven Soloff <s_soloff@users.sourceforge.net>
2013-04-14 03:09:39 +0000

    Added script for launching CruiseControl on a Linux build server.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/build/trunk@3110 897c5806-f142-0410-a293-e1bf5698dec8

---
+++ scripts/cc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# =========================================================================
+#
+# cc.sh
+# Copyright 2008-2013 Gamegineer.org
+# All rights reserved.
+#
+# This is a sample build environment startup script required for a
+# Linux-based Gamegineer build server.  You should download this file to
+# your build server and customize the variable definitions between the
+# BEGIN-CUSTOM-VARIABLES and END-CUSTOM-VARIABLES markers.  The customized
+# file should then be archived on your server and be part of your server's
+# startup process.
+#
+# =========================================================================
+
+# -------------------------------------------------------------------------
+#
+# BEGIN-CUSTOM-VARIABLES
+#
+
+#
+# The build scripts home directory.  This is where your customized build
+# scripts are located.
+#
+export BUILD_SCRIPTS_HOME=~/scripts
+
+#
+# END-CUSTOM-VARIABLES
+#
+# -------------------------------------------------------------------------
+
+#
+# Start CruiseControl.
+#
+source $BUILD_SCRIPTS_HOME/build-environment.sh
+cruisecontrol.sh
