3dd8edc1875ffafe1ea3fed4492ce8b2debac4c0
Steven Soloff <s_soloff@users.sourceforge.net>
2013-06-14 01:50:21 +0000

    Renamed cc.sh to cc-start.sh and added the cc-stop.sh script to stop the build server without having to use the web interface.
    git-svn-id: https://svn.code.sf.net/p/gamegineer/code/build/trunk@3143 897c5806-f142-0410-a293-e1bf5698dec8

--- scripts/cc-stop.sh
+++ scripts/cc-stop.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # =========================================================================
 #
-# cc.sh
+# cc-stop.sh
 # Copyright 2008-2013 Gamegineer.org
 # All rights reserved.
 #
-# This is a sample build environment startup script required for a
+# This is a sample build environment stop script required for a
 # Linux-based Gamegineer build server.  You should download this file to
 # your build server and customize the variable definitions between the
 # BEGIN-CUSTOM-VARIABLES and END-CUSTOM-VARIABLES markers.  The customized
 # file should then be archived on your server and be part of your server's
-# startup process.
+# shutdown process.
 #
 # =========================================================================
 
@@ -23,7 +23,7 @@
 # The build scripts home directory.  This is where your customized build
 # scripts are located.
 #
-export BUILD_SCRIPTS_HOME=~/scripts
+BUILD_SCRIPTS_HOME=~/scripts
 
 #
 # END-CUSTOM-VARIABLES
@@ -34,4 +34,4 @@ export BUILD_SCRIPTS_HOME=~/scripts
 # Start CruiseControl.
 #
 source $BUILD_SCRIPTS_HOME/build-environment.sh
-cruisecontrol.sh
+curl "http://localhost:8000/invoke?operation=halt&objectname=CruiseControl+Manager%3Aid%3Dunique"
